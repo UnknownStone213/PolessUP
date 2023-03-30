3302e4dc683fd1770c89efe99744397e6455a53f
Vlad <vlad.khvesyukovich@gmail.com>
2023-03-30 18:46:33 +0300

    t2 is done

--- code/Properties/AssemblyInfo.cs
+++ code/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("t1")]
+[assembly: AssemblyTitle("code")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("t1")]
+[assembly: AssemblyProduct("code")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4753c761-c612-4dc1-ad9d-46d654501866")]
+[assembly: Guid("2ff5c999-6368-4686-92f9-a64a72053d7d")]
 
 // Version information for an assembly consists of the following four values:
 //
