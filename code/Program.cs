f592d28a21dd67f0c780a25f85cc3a4d3054fc13
Vlad <vlad.khvesyukovich@gmail.com>
2023-03-31 17:28:31 +0300

    t9 is done

--- code/Program.cs
+++ code/Program.cs
@@ -11,27 +11,16 @@ namespace code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the length of array:");
-            int[] nums = new int[Convert.ToInt32(Console.ReadLine())];
-            Console.WriteLine("Fill the array:");
-            for (int i = 0; i < nums.Length; i++)
+            Console.WriteLine("Enter year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
             {
-                Console.Write("nums[{0}] = ", i);
-                nums[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("true");
             }
-            Console.WriteLine("Array has been filled.");
-
-            int index = 0;
-            int max = nums[0];
-            for (int i = 1; i < nums.Length; i++)
+            else 
             {
-                if (max < nums[i])
-                {
-                    max = nums[i];
-                    index = i;
-                }
+                Console.WriteLine("false");
             }
-            Console.WriteLine("Output (position of max element) = " + index);
 
 
             Console.ReadLine();
