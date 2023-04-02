164ba3ed05aa1e8232757f13e37a2e37dd752d02
Vlad <vlad.khvesyukovich@gmail.com>
2023-04-02 18:01:30 +0300

    t7 not done

--- code/Program.cs
+++ code/Program.cs
@@ -14,9 +14,34 @@ namespace code
             Console.WriteLine("Enter number:");
             int num = Convert.ToInt32(Console.ReadLine());
 
+            List<int> list = new List<int>();
 
+            int k1 = Convert.ToString(num).Length;
+
+            //getS(num.ToString(), k1);
 
             Console.ReadLine();
+
+            //void getS(string s, int k) 
+            //{
+            //    if (k == 1)
+            //    {
+            //        list.Add(Convert.ToInt32(s));
+            //    }
+            //    else 
+            //    {
+            //        for (int i = 0; i < 3; i++)
+            //        {
+            //            getS(s, k - 1);
+            //            if (k % 2 == 0)
+            //            {
+            //                char s2 = s[i];
+            //                s[i-1] = s2;
+
+            //            }
+            //        }
+            //    }
+            //}
         }
     }
 }
