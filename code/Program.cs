56d1ebcea54d140ffa447a60933afb7b7082409c
Vlad <vlad.khvesyukovich@gmail.com>
2023-04-02 17:08:45 +0300

    t18 ipv4 is done

--- code/Program.cs
+++ code/Program.cs
@@ -16,13 +16,52 @@ namespace code
             bool ipv4 = checkIPv4(ip);
             bool ipv6 = checkIPv6(ip);
 
-            // if
+            if (ipv4)
+            {
+                Console.WriteLine("Output: IPv4");
+            }
+            else if (ipv6) 
+            {
+                Console.WriteLine("Output: IPv6");
+            }
+            else 
+            {
+                Console.WriteLine("Output: Neither");
+            }
 
             Console.ReadLine();
 
             bool checkIPv4(string s) 
             {
+                int points = -1;
+                string[] split = s.Split('.');
+                points = split.Length;
+                if (points != 4) { return false; }
 
+                bool checkNumbers = true;
+                for (int i = 0; i < split.Length; i++)
+                {
+                    if (Convert.ToInt32(split[i]) < 0 || Convert.ToInt32(split[i]) > 255)
+                    {
+                        checkNumbers = false;
+                    }
+                }
+                if (checkNumbers) 
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            bool checkIPv6(string s) 
+            {
+
+
+
+                return false;
             }
         }
     }
